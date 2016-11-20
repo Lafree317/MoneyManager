67d2ddceb44e28c1ca9645836d60dc0591ec6400
LafreeZhaZha <lafree317@163.com>
2016-11-20 15:18:28 +0800

    搭建框架

--- MoneyManager/Application/App/AppDelegate.swift
+++ MoneyManager/Application/App/AppDelegate.swift
@@ -15,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let noteVC = NoteTableViewController()
+        noteVC.view.backgroundColor = .white
+        let rootNavi = UINavigationController(rootViewController: noteVC)
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = rootNavi
+        self.window = window
+        
         // Override point for customization after application launch.
         return true
     }
---
+++ MoneyManager/Application/UI/Note/NoteCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoteCell.swift
+//  MoneyManager
+//
+//  Created by 胡春源 on 16/11/20.
+//  Copyright © 2016年 Lafree. All rights reserved.
+//
+
+import UIKit
+
+class NoteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
